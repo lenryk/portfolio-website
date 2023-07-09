2769a83a5f4fc923b3a2e9f4d77d5a04fe8bfd5a
Sam <lenry101@gmail.com>
2023-07-09 22:03:15 +0100

    build: initial setup with tailwind

---
+++ src/components/Container/index.js
@@ -0,0 +1,10 @@
+const Container = ({children}) => {
+    return (
+        <main>
+            <h1 className='text-red-300 text-3xl font-bold underline'>test</h1>
+            {children}
+        </main>
+    )
+}
+
+export default Container
