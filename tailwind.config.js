d05e1744774a437df87d52c8aec864f7c3fb6195
Sam <lenry101@gmail.com>
2023-07-09 22:18:40 +0100

    styling: add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,23 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      // https://chir.ag/projects/name-that-color/
+      'primary': '#01080E',
+      'primary-midnight': '#011627',
+      'primary-blue-charcoal': '#011221',
+      'secondary': '#FFFFFF',
+      'secondary-lynch': '#607B96',
+      'secondary-keppel': '#3C9D93',
+      'secondary-indigo': '#4D5BCE',
+      'accent-atomic-tangerine': '#4D5BCE',
+      'accent-turquoise': '#4D5BCE',
+      'accent-tonys-pink': '#E99287',
+      'accent-lavender': '#C98BDF',
+      'lines': '#1E2D3D',
+      'gradient-indigo': '#4D5BCE',
+      'gradient-turquoise': '#43D9AD',
+    },
   },
   plugins: [],
 }
