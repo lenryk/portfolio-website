3c0e56a1268174f178ef90974788076bf85a5c64
Sam <lenry101@gmail.com>
2023-07-12 22:55:32 +0100

    styling: add tech icons to project cards

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -11,6 +11,7 @@ export default function Projects() {
       url: "https://samcarr.co.uk",
       githubUrl: "https://www.github.com/something",
       text: "Portfolio website (the one you are currently on!)",
+      icons: ["react", "next.js", "netlify", "tailwind"],
     },
     gatsby: {
       id: 2,
@@ -18,6 +19,7 @@ export default function Projects() {
       url: "https://samcarr.co.uk",
       githubUrl: "https://www.github.com/something",
       text: "Im test text to see what this would look like in the future",
+      icons: ["react", "next.js"],
     },
     react: {
       id: 3,
@@ -25,6 +27,7 @@ export default function Projects() {
       url: "https://samcarr.co.uk",
       githubUrl: "https://www.github.com/something",
       text: "Another amazing project but with more filler text",
+      icons: ["netlify", "tailwind"],
     },
   };
 
@@ -32,19 +35,19 @@ export default function Projects() {
     <section className="flex h-full">
       <div>
         <div className="flex h-[41px] w-[311px] items-center gap-[11px] border-b border-r border-lines pl-5">
-          <Icon icon="folder-green" size={24} /> projects
+          <Icon icon="folder-green" size={24} /> tech-filter
         </div>
         <div className="h-full w-[311px] border-r border-lines">
           <Filters />
         </div>
       </div>
-      <div className="flex w-full flex-col">
+      <div className="flex w-full flex-col ">
         <div className="flex h-[41px] w-full border-b border-lines">
           <div className="flex h-[41px] w-fit items-center gap-[48px] border-r border-lines pl-3.5 pr-3 ">
-            portfolio-final.jsx <Icon icon="close" size={20} />
+            projects-final.jsx <Icon icon="close" size={20} />
           </div>
         </div>
-        <div className="flex gap-10">
+        <div className="flex h-full items-center justify-center gap-10">
           {Object.entries(projects).map(([name, metadata]) => (
             <Card key={metadata.id} name={name} metadata={metadata} />
           ))}
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,4 +1,6 @@
 import Button from "@/components/Button";
+import Icon from "@/components/Icon";
+import { clsx } from "clsx";
 
 export default function Card({ name, metadata }) {
   return (
@@ -10,7 +12,28 @@ export default function Card({ name, metadata }) {
         <span className="text-secondary-lynch"> // _{name}</span>
       </p>
       <div className="flex w-[370px] flex-col overflow-hidden rounded-2xl border border-lines">
-        <div className="h-[145px] w-full bg-secondary-lynch"></div>
+        <div className="border-1 relative h-[145px] w-full border-b border-lines bg-primary-blue-charcoal">
+          <div className="absolute bottom-0 left-0 mb-2 ml-2 flex gap-2">
+            {metadata.icons.map((icon) => {
+              const iconBackground = clsx("rounded-sm p-1", {
+                "bg-gradient-turquoise": icon === "react",
+                "bg-button-hover": icon === "next.js",
+                "bg-accent-tonys-pink": icon === "netlify",
+                "bg-secondary-indigo": icon === "tailwind",
+              });
+
+              return (
+                <div
+                  title={icon.charAt(0).toUpperCase() + icon.slice(1)}
+                  key={`${name}-${icon}`}
+                  className={iconBackground}
+                >
+                  <Icon alt={`${name}ProjectIcon`} icon={icon} size={20} />
+                </div>
+              );
+            })}
+          </div>
+        </div>
         <div className="px-8 py-6">
           <p className="text-secondary-lynch">{metadata.text}</p>
           <div className="flex gap-5 pt-5">
